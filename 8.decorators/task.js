0cce3a8a75f569da828840eda6c67ecffa7c93e8
unknown <evshkryabiy@google.com>
2022-03-30 17:49:04 +0300

    HW8 1 task done

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,7 +1,24 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  const cache = [];
+  return function(...rest){
+    let key = rest.toString()    
+    let idx = cache.findIndex((item)=> item[key] !== undefined); 
+    if (idx !== -1 ) {
+      let show = cache[idx];
+      
+      console.log("Из кэша: " + show[key]);
+     return "Из кэша: " + show[key];
+    }else{
+     let result = func(...rest); 
+     cache.push({[key] : result})
+     if (cache.length > 5){
+       cache.shift();       
+     }
+      console.log("Вычисляем: " + result);
+      return "Вычисляем: " + result;
+    }
+  }
 }
-
 
 function debounceDecoratorNew(func) {
   // Ваш код
