2f80edd09b86cbdfafc4c6d1349f4c54f2626e17
unknown <evshkryabiy@google.com>
2022-04-01 17:37:52 +0300

    code changed

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -20,27 +20,39 @@ function cachingDecoratorNew(func) {
   }
 }
 
-function debounceDecoratorNew(func, ms) {  
+function debounceDecoratorNew(func, ms) {
+  let flag = false;  
   let timeout;
   function wrapper(...rest){    
     clearTimeout(timeout);
-    func.apply(this, rest);
-    timeout = setTimeout(() => {
-      func.apply(this, rest);      
-    }, ms)
+    if (flag === false){
+      func.apply(this, rest);
+      flag = true;
+    }else{    
+      timeout = setTimeout(() => {
+        func.apply(this, rest);
+        flag = false;      
+      }, ms)
+    }
   }
   return wrapper;
 }
 
 function debounceDecorator2(func, ms) {
   let timeout;
+  let flag = false;
   function wrapper(...rest){          
-    clearTimeout(timeout);  
-    func.apply(this, rest);   
-    timeout = setTimeout(() => {
+    clearTimeout(timeout);
+    if (flag === false){
       func.apply(this, rest);
-      console.log(`Декоратор вызван ${wrapper.count.length} раз`)      
-    }, ms)
+      flag = true;
+    }else{  
+      timeout = setTimeout(() => {
+        func.apply(this, rest);
+        flag = false;  
+        console.log(`Декоратор вызван ${wrapper.count.length} раз`)      
+      }, ms)
+    }
     wrapper.count.push(rest);    
   }
   wrapper.count = [];  
